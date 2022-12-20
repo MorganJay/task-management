b5f2445dac877bc0b1572b3bc23c4c01f0373a23
James Morgan <MorganJay@users.noreply.github.com>
2022-12-20 15:47:12 +0100

    deployment changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,16 +19,24 @@ import { configValidationSchema } from './config.schema';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
-        autoLoadEntities: true,
-        synchronize: true,
-        host: configService.get('DB_HOST'),
-        username: configService.get('DB_USERNAME'),
-        port: configService.get('DB_PORT'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-      }),
+      useFactory: async (configService: ConfigService) => {
+        const isProduction = process.env.NODE_ENV === 'production';
+
+        return {
+          ssl: isProduction,
+          extra: {
+            ssl: isProduction ? { rejectUnauthorized: false } : null,
+          },
+          type: 'postgres',
+          autoLoadEntities: true,
+          synchronize: true,
+          host: configService.get('DB_HOST'),
+          username: configService.get('DB_USERNAME'),
+          port: configService.get('DB_PORT'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_NAME'),
+        };
+      },
     }),
     // TypeOrmModule.forRoot(typeOrmConfig),
     TasksModule,
--- src/config.schema.ts
+++ src/config.schema.ts
@@ -1,7 +1,7 @@
 import * as Joi from '@hapi/joi';
 
 export const configValidationSchema = Joi.object({
-  NODE_ENV: Joi.string().required(),
+  PORT: Joi.number().default(3000),
   DB_HOST: Joi.string().required(),
   DB_PORT: Joi.number().default(5432).required(),
   DB_USERNAME: Joi.string().required(),
